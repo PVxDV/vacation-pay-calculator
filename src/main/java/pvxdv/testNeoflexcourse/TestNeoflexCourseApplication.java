77ba18609352b5c6aa3c23a4361627811c08e781
PVxDV <rizhikh.pavel@yandex.ru>
2023-05-15 21:54:38 +0300

    first commit test

---
+++ src/main/java/pvxdv/testNeoflexcourse/TestNeoflexCourseApplication.java
@@ -0,0 +1,13 @@
+package pvxdv.testNeoflexcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNeoflexCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNeoflexCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pvxdv/testNeoflexcourse/TestNeoflexCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package pvxdv.testNeoflexcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNeoflexCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
